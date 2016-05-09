543c63dbb438de480fad86544f745871aa19ee21
JoeCZ <zhangchuou@gmail.com>
2016-05-08 20:27:57 -0700

    Created Repo

---
+++ SUsersList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SUsersList
+//
+//  Created by Chuou Zhang on 5/8/16.
+//  Copyright © 2016 Chuou Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SUsersListTests/SUsersListTests.m
@@ -0,0 +1,39 @@
+//
+//  SUsersListTests.m
+//  SUsersListTests
+//
+//  Created by Chuou Zhang on 5/8/16.
+//  Copyright © 2016 Chuou Zhang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SUsersListTests : XCTestCase
+
+@end
+
+@implementation SUsersListTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
